3e5fc006c96f30fd0beac5309b22e6f22b2df2db
Zar <tsmywka@gmail.com>
2022-08-17 21:38:35 +0300

    hw_25

---
+++ app/src/main/java/com/mtsarkov/firstapp/BlankFragment.kt
@@ -0,0 +1,53 @@
+package com.mtsarkov.firstapp
+
+import TopSpacingItemDecoration
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_blank.*
+
+class BlankFragment : Fragment() {
+    private lateinit var filmsAdapter: FilmListRecyclerAdapter
+
+    val filmsDataBase = listOf(
+        Film("Prey", R.drawable.prey, "The origin story of the Predator in the world of the Comanche Nation 300 years ago. Naru, a skilled warrior, fights to protect her tribe against one of the first highly-evolved Predators to land on Earth."),
+        Film("Bullet Train", R.drawable.bullet_train, "Five assassins aboard a fast moving bullet train find out their missions have something in common."),
+        Film("Thirteen Lives", R.drawable.thirteen_lives, "A rescue mission is assembled in Thailand where a group of young boys and their soccer coach are trapped in a system of underground caves that are flooding."),
+        Film("Purple Hearts", R.drawable.purple_hearts, "In spite of their many differences, Cassie, a struggling singer-songwriter, and Luke, a troubled Marine, agree to marry solely for military benefits. But when tragedy strikes, the line between real and pretend begins to blur."),
+        Film("Nope", R.drawable.nope, "The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery."),
+        Film("The Gray Man", R.drawable.the_gray_man, "When the CIA's most skilled operative-whose true identity is known to none-accidentally uncovers dark agency secrets, a psychopathic former colleague puts a bounty on his head, setting off a global manhunt by international assassins."),
+        Film("Elvis", R.drawable.elvis, "The life of American music icon Elvis Presley, from his childhood to becoming a rock and movie star in the 1950s while maintaining a complex relationship with his manager, Colonel Tom Parker."),
+        Film("Day Shift", R.drawable.day_shift, "A hard-working, blue-collar dad who just wants to provide a good life for his quick-witted 8-year-old daughter. His mundane San Fernando Valley pool cleaning job is a front for his real source of income: hunting and killing vampires."),
+        Film("Uncharted", R.drawable.uncharted, "Street-smart Nathan Drake is recruited by seasoned treasure hunter Victor \"Sully\" Sullivan to recover a fortune amassed by Ferdinand Magellan, and lost 500 years ago by the House of Moncada."),
+        Film("Lightyear", R.drawable.lightyear, "While spending years attempting to return home, marooned Space Ranger Buzz Lightyear encounters an army of ruthless robots commanded by Zurg who are attempting to steal his fuel source.")
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        main_recycler.apply {
+            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
+                override fun click(film: Film) {
+                    (requireActivity() as MainActivity).launchDetailsFragment(film)
+                }
+            })
+            adapter = filmsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
+        }
+        filmsAdapter.addItems(filmsDataBase)
+    }
+
+}
--- app/src/main/java/com/mtsarkov/firstapp/MainActivity.kt
+++ app/src/main/java/com/mtsarkov/firstapp/MainActivity.kt
@@ -1,38 +1,58 @@
 package com.mtsarkov.firstapp
 
-import TopSpacingItemDecoration
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import android.content.res.Resources
-import android.graphics.Rect
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var filmsAdapter: FilmListRecyclerAdapter
-
-    val filmsDataBase = listOf(
-        Film("Prey", R.drawable.prey, "The origin story of the Predator in the world of the Comanche Nation 300 years ago. Naru, a skilled warrior, fights to protect her tribe against one of the first highly-evolved Predators to land on Earth."),
-        Film("Bullet Train", R.drawable.bullet_train, "Five assassins aboard a fast moving bullet train find out their missions have something in common."),
-        Film("Thirteen Lives", R.drawable.thirteen_lives, "A rescue mission is assembled in Thailand where a group of young boys and their soccer coach are trapped in a system of underground caves that are flooding."),
-        Film("Purple Hearts", R.drawable.purple_hearts, "In spite of their many differences, Cassie, a struggling singer-songwriter, and Luke, a troubled Marine, agree to marry solely for military benefits. But when tragedy strikes, the line between real and pretend begins to blur."),
-        Film("Nope", R.drawable.nope, "The residents of a lonely gulch in inland California bear witness to an uncanny and chilling discovery."),
-        Film("The Gray Man", R.drawable.the_gray_man, "When the CIA's most skilled operative-whose true identity is known to none-accidentally uncovers dark agency secrets, a psychopathic former colleague puts a bounty on his head, setting off a global manhunt by international assassins."),
-        Film("Elvis", R.drawable.elvis, "The life of American music icon Elvis Presley, from his childhood to becoming a rock and movie star in the 1950s while maintaining a complex relationship with his manager, Colonel Tom Parker."),
-        Film("Day Shift", R.drawable.day_shift, "A hard-working, blue-collar dad who just wants to provide a good life for his quick-witted 8-year-old daughter. His mundane San Fernando Valley pool cleaning job is a front for his real source of income: hunting and killing vampires."),
-        Film("Uncharted", R.drawable.uncharted, "Street-smart Nathan Drake is recruited by seasoned treasure hunter Victor \"Sully\" Sullivan to recover a fortune amassed by Ferdinand Magellan, and lost 500 years ago by the House of Moncada."),
-        Film("Lightyear", R.drawable.lightyear1, "While spending years attempting to return home, marooned Space Ranger Buzz Lightyear encounters an army of ruthless robots commanded by Zurg who are attempting to steal his fuel source.")
-    )
-
+    private var backPressed = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initTopBar()
+        initNavigation()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_placeholder, BlankFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 1){
+            if (backPressed + EXIT_TIME_INTERVAL > System.currentTimeMillis()){
+                super.onBackPressed()
+                finish()
+            } else {
+                Toast.makeText(this, R.string.message_fro_exit, Toast.LENGTH_SHORT).show()
+            }
+            backPressed = System.currentTimeMillis()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        const val EXIT_TIME_INTERVAL = 2000
+    }
+
+    fun launchDetailsFragment(film: Film) {
+        val bundle = Bundle()
+        bundle.putParcelable("film", film)
+        val fragment = DetailsFragment()
+        fragment.arguments = bundle
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun initTopBar(){
         topAppBar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.settings -> {
@@ -42,38 +62,10 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
 
-        main_recycler.apply {
-            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
-            //оставим его пока пустым, он нам понадобится во второй части задания
-            filmsAdapter = FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener{
-                override fun click(film: Film) {
-                    //Создаем бандл и кладем туда объект с данными фильма
-                    val bundle = Bundle()
-                    //Первым параметром указывается ключ, по которому потом будем искать, вторым сам
-                    //передаваемый объект
-                    bundle.putParcelable("film", film)
-                    //Запускаем наше активити
-                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-                    //Прикрепляем бандл к интенту
-                    intent.putExtras(bundle)
-                    //Запускаем активити через интент
-                    startActivity(intent)
-                }
-            })
-            //Присваиваем адаптер
-            adapter = filmsAdapter
-            //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            //Применяем декоратор для отступов
-            val decorator = TopSpacingItemDecoration(8)
-            addItemDecoration(decorator)
-        }
-        //Кладем нашу БД в RV
-        filmsAdapter.addItems(filmsDataBase)
-
+    private fun initNavigation() {
         bottom_navigation.setOnItemReselectedListener {
-
             when (it.itemId) {
                 R.id.favorites -> {
                     Toast.makeText(this, R.string.favorite, Toast.LENGTH_SHORT).show()
