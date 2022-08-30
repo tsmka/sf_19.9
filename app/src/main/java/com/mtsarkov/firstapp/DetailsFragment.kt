db56e75faec4dcac7f8174be6b637b766b21f34a
Zar <tsmywka@gmail.com>
2022-08-30 18:07:34 +0300

    hm_26

--- app/src/main/java/com/mtsarkov/firstapp/DetailsFragment.kt
+++ app/src/main/java/com/mtsarkov/firstapp/DetailsFragment.kt
@@ -1,13 +1,17 @@
 package com.mtsarkov.firstapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_details.*
+import kotlinx.android.synthetic.main.fragment_favorites.*
 
 class DetailsFragment : Fragment() {
+
+    private lateinit var film : Film
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,14 +25,40 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setFilmsDetails()
+
+        details_fab_favorites.setOnClickListener {
+            if (!film.isInFavorites) {
+                details_fab_favorites.setImageResource(R.drawable.ic_round_favorite_24)
+                film.isInFavorites = true
+            } else {
+                details_fab_favorites.setImageResource(R.drawable.ic_round_favorite_border_24)
+                film.isInFavorites = false
+            }
+        }
+
+        details_fab_share.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Check out this film: ${film.title} \n\n ${film.description}"
+            )
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share To:"))
+        }
     }
 
     private fun setFilmsDetails() {
-        val film = arguments?.get("film") as Film
+        film = arguments?.get("film") as Film
 
         details_toolbar.title = film.title
         details_poster.setImageResource(film.poster)
         details_description.text = film.description
+
+        details_fab_favorites.setImageResource(
+            if (film.isInFavorites) R.drawable.ic_round_favorite_24
+            else R.drawable.ic_round_favorite_border_24
+        )
     }
 }
 
